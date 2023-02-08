9180a4572c762487d2e216427b9cc6c5f5bed604
AKOKA <akoka@et.esiea.fr>
2023-02-08 14:01:42 +0100

    Librairie moteur - Ajout fonction cmd_motors

--- SYS4041_ROBOTIQUE/lib/TCC-motor/TCC-motor.cpp
+++ SYS4041_ROBOTIQUE/lib/TCC-motor/TCC-motor.cpp
@@ -1,23 +1,30 @@
 #include <TCC-motor.h>
-
+#include "Arduino.h"
 
 void init_motorAB(){
-  pinMode(pinMotorDir_A, OUTPUT);
-  pinMode(pinMotorPWM_A, OUTPUT);
-  pinMode(pinMotorDir_B, OUTPUT);
-  pinMode(pinMotorPWM_B, OUTPUT);
+    pinMode(pinMotorDir_A, OUTPUT);
+    pinMode(pinMotorPWM_A, OUTPUT);
+    pinMode(pinMotorDir_B, OUTPUT);
+    pinMode(pinMotorPWM_B, OUTPUT);
 }
 
 void motorA_setDir(bool dir){ //HIGH en avant LOW en arrière
-  digitalWrite(pinMotorDir_A, dir);
+    digitalWrite(pinMotorDir_A, dir);
 }
 
-void motorA_setPWM(int pwm){
-  digitalWrite(pinMotorPWM_A, pwm);
+void motorA_setPWM(int pwm){ //0-255 pour choisir entre moteur arrêt jusqu'à moteur alimenté en puissance max
+    digitalWrite(pinMotorPWM_A, pwm);
 }
 void motorB_setDir(bool dir){
-  digitalWrite(pinMotorDir_B, dir);
+    digitalWrite(pinMotorDir_B, dir);
 }
 void motorB_setPWM(int pwm){
-  digitalWrite(pinMotorDir_B, pwm);
+    digitalWrite(pinMotorDir_B, pwm);
+}
+
+void cmd_motors(int pwmD, int pwmG){
+    digitalWrite(pinMotorDir_A, (pwmG>0));
+    digitalWrite(pinMotorPWM_A, (pwmG>0)?pwmG:(-pwmG));
+    digitalWrite(pinMotorDir_B, (pwmD>0));
+    digitalWrite(pinMotorDir_B, (pwmD>0)?pwmG:(-pwmD));
 }
--- SYS4041_ROBOTIQUE/lib/TCC-motor/TCC-motor.h
+++ SYS4041_ROBOTIQUE/lib/TCC-motor/TCC-motor.h
@@ -1,11 +1,15 @@
+
 #define pinMotorDir_A 2
 #define pinMotorPWM_A 3
 #define pinMotorDir_B 8
 #define pinMotorPWM_B 9
 
 void motorA_setDir(int dir);
+
 void motorA_setPWM(int pwm);
+
 void motorB_setDir(int dir);
+
 void motorB_setPWM(int pwm);
 
-
+void cmd_motors(int pwmD, int pwmG);
