a540fc3fba45fdabce281d698738112036d17bec
AKOKA <akoka@et.esiea.fr>
2023-02-09 11:32:17 +0100

    modification asservissement

--- SYS4041_ROBOTIQUE/src/main.cpp
+++ SYS4041_ROBOTIQUE/src/main.cpp
@@ -6,6 +6,10 @@
 
 //--------------------------------Constante - Variables globales
 
+float asservAP = -0.3, asservAI = 0.1, 
+        asservLP = 0.8, asservLI = 0,
+        somErrA = 0, somErrL = 0;
+
 HUSKYLENS huskylens; //HUSKYLENS green line >> SDA; blue line >> SCL
 enum state_e {
     IDLE,
@@ -24,6 +28,8 @@ bool isTag(int indexTag);
 HUSKYLENSResult getTag(int indexTag);
 bool delayState (int delaytime);
 void newState(state_e newE);
+void followTag(int IDTag, int consigneCentre, int consigneDist);
+void followTagAA(int IDTag, int consigneCentre, int consigneDist);
 
 //--------------------------------SETUP
 void setup() {
@@ -39,45 +45,8 @@ void setup() {
     init_motorAB();
 }
 //--------------------------------LOOP
-float asservAP = 0.3, asservAI = 0.3, somErrA = 0, somErrL = 0, asservLP = 0.7, asservLI = 0.1;
 void loop() {
-    HUSKYLENSResult tag = getTag(2);
-    if(tag.ID != -1){
-      //printResult(tag);
-      int consigne = 160;
-      int input = tag.xCenter;
-      int erreur = consigne - input;
-      int output = (int)((float)(erreur * asservAP)) + (int)(float)(somErrA * asservAI);
-      
-      if(input == 160)
-        somErrA = 0;
-      else 
-        somErrA += erreur;
-
-      int input2 = tag.height;
-      int consigne2 = 180;
-      int erreur2 = consigne2 - input2;
-      int output2 = (int)((float)(erreur2 * asservLP)) + (int)(float)(somErrL * asservLI);
-      //Serial.println(output2);Serial.println(" / ");Serial.println(output);
-      output = max(output, (-255));
-      output = min(output, 255);
-      output2 = max(output2, -255);
-      output2 = min(output2, 255);
-      if(input2 >= 180){
-        somErrL = 0;
-        output2 *= (-1);
-      }
-      else {
-        somErrL += erreur2;
-      }
-      
-      cmd_robot(output2, output);
-    }
-    else {
-      Serial.println("Mauvais tag");
-      cmd_robot(0, 0);
-    }
-
+  followTag(2, 160, 180);
   /*switch (state)
   {
       case IDLE :
@@ -105,6 +74,34 @@ void loop() {
 
 
 
+}
+
+void followTag(int IDTag, int consigneCentre, int consigneDist){// 2 160 190
+  HUSKYLENSResult tag = getTag(IDTag);
+  if(tag.ID != -1){
+    int input = tag.xCenter;
+    int erreur = consigneCentre - input;
+    int output = (int)((float)(erreur * asservAP));
+    output = max(output, (-255));
+    output = min(output, 255);
+
+    int input2 = tag.height;
+    int erreur2 = consigneDist - input2;
+    int output2 = (int)((float)(erreur2 * asservLP));
+  
+    output2 = max(output2, -255);
+    output2 = min(output2, 255);
+    if(input2 >= 180){
+      output2 = output2*(-1);
+    }
+    
+    
+    cmd_robot(output2, output);
+  }
+  else {
+    Serial.println("Mauvais tag");
+    cmd_robot(0, 0);
+  }
 }
 
 void printResult(HUSKYLENSResult result){
