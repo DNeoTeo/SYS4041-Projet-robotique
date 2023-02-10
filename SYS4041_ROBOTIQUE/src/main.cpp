dc851b88124da956b713176ee4e6823cf76a2870
AKOKA <akoka@et.esiea.fr>
2023-02-10 11:08:55 +0100

    Debut d'optimisation des deplacements

--- SYS4041_ROBOTIQUE/src/main.cpp
+++ SYS4041_ROBOTIQUE/src/main.cpp
@@ -9,14 +9,16 @@
 TCC_Huskylens huskylens;
 TCC_Motor motor;
 float asservAP = -0.3, asservAI = 0.1, 
-        asservLP = 0.8, asservLI = 0,
+        asservLP = 0.9, asservLI = 0,
         somErrA = 0, somErrL = 0;
-#define MAX_HEIGHT_ARUCO 180
+#define MAX_HEIGHT_ARUCO 170
+#define MAX_NB_TAG 7
 // Variables
   enum state_e {
     IDLE,
     START,
     LOOK_FOR_TAG,
+    FOLLOW_TAG,
     TAG,
     STOP
   };
@@ -32,7 +34,6 @@ float asservAP = -0.3, asservAI = 0.1,
 bool delayState (int delaytime);
 void newState(state_e newE);
 void followTag(int IDTag, int consigneCentre, int consigneDist);
-void followTagAA(int IDTag, int consigneCentre, int consigneDist);
 void stateMachine();
 
 /********** Setup *********/
@@ -45,8 +46,6 @@ void setup() {
 /********** Loop *********/
 void loop() {
     stateMachine();
-    followTag(2, 160, 180);
-
 }
 
 /********** Asservissement poursuite de Tag *********/
@@ -65,9 +64,9 @@ void followTag(int IDTag, int consigneCentre, int consigneDist){// 2 160 190
   
     output2 = max(output2, -255);
     output2 = min(output2, 255);
-    if(input2 >= consigneDist){
+    /*if(input2 >= consigneDist){
       output2 = output2*(-1);
-    }
+    }*/
     
     
     motor.cmd_robot(output2, output);
@@ -94,49 +93,81 @@ void stateMachine() {
 
     case START :
       //c'est vert, on démarre et on va dans look for tag
-      TagNbr = 7;
+      TagNbr = 1;
       motor.cmd_robot(255,0);
       if(delayState(800)) {
         motor.cmd_robot(0,0);
       }
       huskylens.huskylens.writeAlgorithm(ALGORITHM_TAG_RECOGNITION);
       if(delayState(1000)) {
-        newState(LOOK_FOR_TAG);
+        newState(TAG);
       }
       break;
     
     case LOOK_FOR_TAG :
       // on cherche le tag
-      cmd_robot(0, 0);
-      if(huskylens.isTag(TagNbr) && delayState(100)) {
-        newState(TAG);
+      motor.cmd_robot(0, 40);
+      if(huskylens.isTag(TagNbr)) {
+        newState(FOLLOW_TAG);
       }
-      
       break;
     
-    case TAG :
-      //on incrémente et on revient dans look for tag
-       switch (TagNbr)
-      {
-      case 1:
-        /* code */
-        break;
-      
-      default:
-        break;
-      }
-      followTag(TagNbr, 160, MAX_HEIGHT_ARUCO);
-      if (TagNbr <= 7) {
-        followTag(TagNbr, 160, MAX_HEIGHT_ARUCO);
-        if(huskylens.getTag(TagNbr).height >= MAX_HEIGHT_ARUCO){
-          newState(LOOK_FOR_TAG);
+    case FOLLOW_TAG : 
+      if (TagNbr <= MAX_NB_TAG){
+        followTag(TagNbr, 160, MAX_HEIGHT_ARUCO+30);
+        int heightTAG = huskylens.getTag(TagNbr).height;
+        //Serial.print("HEIGHT= ");
+      //Serial.println(heightTAG);
+        if(heightTAG >= 140){
           TagNbr ++;
+          newState(TAG);
         }
       }
-      else {
-          newState(STOP);
+      else{
+        newState(STOP);
       }
-      
+    break;
+
+    case TAG :
+      //on incrémente et on revient dans look for tag
+      //Serial.print("Tag nb= ");
+      //Serial.println(TagNbr);
+       switch (TagNbr)
+      {
+        case 1:
+          motor.cmd_robot(255,0);
+          if(delayState(1000)){
+            newState(LOOK_FOR_TAG);
+          }
+          break;
+        case 2:
+          motor.cmd_robot(50,200);
+          if(delayState(700)){
+            newState(LOOK_FOR_TAG);
+          }
+          break;
+        case 3:
+          motor.cmd_robot(50,-200);
+          if(delayState(400)){
+            newState(LOOK_FOR_TAG);
+          }
+          break;
+        case 4:
+          /* code */
+          break;
+        case 5:
+          /* code */
+          break;
+        case 6:
+          /* code */
+          break;
+        case 7:
+
+          break;
+        default:
+          newState(LOOK_FOR_TAG);
+          break;
+      }      
       break;
 
     case STOP :
