f1f06c21a22f6bbff88af966cba2a86835216167
AKOKA <akoka@et.esiea.fr>
2023-02-10 09:05:00 +0100

    main sans prob - avant test sur robot

--- SYS4041_ROBOTIQUE/src/main.cpp
+++ SYS4041_ROBOTIQUE/src/main.cpp
@@ -10,7 +10,19 @@ TCC_Huskylens huskylens;
 float asservAP = -0.3, asservAI = 0.1, 
         asservLP = 0.8, asservLI = 0,
         somErrA = 0, somErrL = 0;
+#define MAX_HEIGHT_ARUCO 185
+// Variables
+  enum state_e {
+    IDLE,
+    START,
+    LOOK_FOR_TAG,
+    TAG,
+    STOP
+  };
 
+  state_e state = IDLE;
+  long last_millis;
+  int TagNbr = 0;
 
 /********** Functions *********/
 //void printResult(HUSKYLENSResult result);
@@ -20,7 +32,8 @@ bool delayState (int delaytime);
 void newState(state_e newE);
 void followTag(int IDTag, int consigneCentre, int consigneDist);
 void followTagAA(int IDTag, int consigneCentre, int consigneDist);
-
+void stateMachine();
+bool checkcolor();
 
 /********** Setup *********/
 void setup() {
@@ -30,16 +43,15 @@ void setup() {
 }
 
 /********** Loop *********/
-float asservAP = 0.3, asservAI = 0.3, somErrA = 0, somErrL = 0, asservLP = 0.7, asservLI = 0.1;
-//--------------------------------LOOP
 void loop() {
     stateMachine();
     followTag(2, 160, 180);
 
 }
 
+/********** Asservissement poursuite de Tag *********/
 void followTag(int IDTag, int consigneCentre, int consigneDist){// 2 160 190
-  HUSKYLENSResult tag = getTag(IDTag);
+  HUSKYLENSResult tag = huskylens.getTag(IDTag);
   if(tag.ID != -1){
     int input = tag.xCenter;
     int erreur = consigneCentre - input;
@@ -53,7 +65,7 @@ void followTag(int IDTag, int consigneCentre, int consigneDist){// 2 160 190
   
     output2 = max(output2, -255);
     output2 = min(output2, 255);
-    if(input2 >= 180){
+    if(input2 >= consigneDist){
       output2 = output2*(-1);
     }
     
@@ -69,19 +81,6 @@ void followTag(int IDTag, int consigneCentre, int consigneDist){// 2 160 190
 
 /********** State Machine *********/
 void stateMachine() {
-  // Variables
-  enum state_e {
-    IDLE,
-    START,
-    LOOK_FOR_TAG,
-    TAG,
-    STOP
-  };
-
-  state_e state = IDLE;
-  long last_millis;
-  int TagNbr;
-
   // Switch case
   switch (state)
   {
@@ -90,7 +89,7 @@ void stateMachine() {
       cmd_robot(0,0);
       if (checkcolor()) {
         if(delayState(1000)) {
-            newState(START);
+          newState(START);
         }
       }
       break;
@@ -98,28 +97,32 @@ void stateMachine() {
     case START :
       //c'est vert, on démarre et on va dans look for tag
       TagNbr = 1;
-      huskylens.writeAlgorithm (ALGORITHM_TAG_RECOGNITION);
+      cmd_robot(255,0);
+      if(delayState(800)) {
+        cmd_robot(0,0);
+      }
+      //huskylens.writeAlgorithm(ALGORITHM_TAG_RECOGNITION);
       if(delayState(1000)) {
-          newState(LOOK_FOR_TAG);
+        newState(LOOK_FOR_TAG);
       }
       break;
     
     case LOOK_FOR_TAG :
       // on cherche le tag
-      if (isTag(TagNbr)) {
-        if(delayState(1000)) {
-              newState(TAG);
-        }
+      if(huskylens.isTag(TagNbr)) {
+        newState(TAG);
       }
+      
       break;
     
     case TAG :
       //on incrémente et on revient dans look for tag
-      TagNbr ++;
-      if (TagNbr <= 5) {
-        if(delayState(1000)) {
-              newState(LOOK_FOR_TAG);
+      if (TagNbr <= 6) {
+        followTag(TagNbr, 160, MAX_HEIGHT_ARUCO);
+        if(huskylens.getTag(TagNbr).height >= MAX_HEIGHT_ARUCO){
+          newState(LOOK_FOR_TAG);
         }
+        TagNbr ++;
       }
       else {
         if(delayState(1000)) {
@@ -139,12 +142,15 @@ void stateMachine() {
   }
 }
 
-void newState (state new) {
-    //Serial.println(new);
-    state = new;
-    last.millis = millis();
+void newState (state_e newE) {
+    state = newE;
+    last_millis = millis();
 }
 
 bool delayState(int delaytime){
-    return((millis().last.millis)>= delaytime);
+    return((int)(millis()-last_millis)>= delaytime);
+}
+
+bool checkcolor(){
+  return true;
 }
